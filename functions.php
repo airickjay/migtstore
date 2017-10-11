d142848663915d924957b8f6b72d4786269df4bd
Eric Samson <samsone@rhs.msu.edu>
2017-10-11 16:24:16 -0400

    functions adds

--- functions.php
+++ functions.php
@@ -2,4 +2,12 @@
 
 $timber = new Timber\Timber();
 
+function migtStore_include_custom_jquery() {
+
+	wp_deregister_script('jquery');
+	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), null, true);
+
+}
+add_action('wp_enqueue_scripts', 'migtStore_include_custom_jquery');
+
 ?>
