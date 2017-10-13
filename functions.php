aeed179f70bc31a6f9de7afc3e7be35c1a52d765
Eric Samson <samsone@rhs.msu.edu>
2017-10-13 14:53:52 -0400

    More theme improvements

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 function migtStore_include_foundation() {
  
- 	wp_deregister_script('jquery');
-	wp_enqueue_script('jquery', '/wp-content/themes/migtstore/foundation/js/vendor/jquery.js', array(), null, true);
+ 	wp_enqueue_script('jquery');
+	//wp_enqueue_script('jquery', '/wp-content/themes/migtstore/foundation/js/vendor/jquery.js', array(), null, true);
 	wp_enqueue_script('foundation', '/wp-content/themes/migtstore/foundation/js/vendor/foundation.min.js', array(), null, true);
 	wp_enqueue_script('what_input', '/wp-content/themes/migtstore/foundation/js/vendor/what-input.js', array(), null, true);
 	wp_enqueue_script('foundation_app','/wp-content/themes/migtstore/foundation/js/app.js', array('jquery'), null, true);
@@ -10,4 +10,6 @@ function migtStore_include_foundation() {
  }
 
 add_action('wp_enqueue_scripts', 'migtStore_include_foundation');
+
+
 ?>
