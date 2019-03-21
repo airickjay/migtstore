62018ff78580a7258c1c0a0c41532b7e119ced8b
Eric Samson <samsone@user-2f68f6.user.msu.edu>
2019-03-21 14:07:10 -0400

    removed foundation from theme

--- functions.php
+++ functions.php
@@ -1,15 +1,4 @@
 <?php
-function migtStore_include_foundation() {
-
- 	wp_enqueue_script('jquery');
-	//wp_enqueue_script('jquery', '/wp-content/themes/migtstore/foundation/js/vendor/jquery.js', array(), null, true);
-	wp_enqueue_script('foundation', '/wp-content/themes/migtstore/foundation/js/vendor/foundation.min.js', array(), null, true);
-	wp_enqueue_script('what_input', '/wp-content/themes/migtstore/foundation/js/vendor/what-input.js', array(), null, true);
-	wp_enqueue_script('foundation_app','/wp-content/themes/migtstore/foundation/js/app.js', array('jquery'), null, true);
-
- }
-
-add_action('wp_enqueue_scripts', 'migtStore_include_foundation');
 
 include_once('shortcodes/event_data.php');
 
